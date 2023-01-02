330880d317a61be6b89c50668d0155405e8df2ef
Manoj Joshi <hello_manoj@outlook.com>
2023-01-02 13:52:10 +0545

    up2

--- YTD/main.js
+++ YTD/main.js
@@ -4,7 +4,8 @@ var dwnbtn = document.querySelector(".result2");
 var r1 = document.querySelector(".result1");
 var r2 = document.querySelector(".result2");
 var rm = document.getElementById("rmsg");
-
+var css = "https://hello-manoj.github.io/extracss/api.css";
+var buttonColor = "ff255c";
 function download() {
   if (link.value != "") {
     rm.style.visibility = "visible";
@@ -14,7 +15,7 @@ function download() {
         "https://www.youtube.com/embed/"
       );
       r1.innerHTML = `<iframe id="myframe" width="105%" height="105%" src="${url1}" encrypted-media; allowfullscreen></iframe>`;
-      var url2 = `https://loader.to/api/button/?url=${link.value}&f=${format.value}`;
+      var url2 = `https://loader.to/api/button/?url=${link.value}&f=${format.value}&color=${buttonColor}&css=${css}`;
       r2.innerHTML += `<iframe scrolling="no" src="${url2}"></iframe>`;
       dwnbtn.style.backgroundColor = "#ff255c";
     } else if (link.value.indexOf("https://www.youtube.com/watch?v=") != -1) {
@@ -27,7 +28,7 @@ function download() {
         "https://www.youtube.com/watch?v=",
         "https://youtu.be/"
       );
-      var url2 = `https://loader.to/api/button/?url=${u}&f=${format.value}`;
+      var url2 = `https://loader.to/api/button/?url=${u}&f=${format.value}&color=${buttonColor}&css=${css}`;
       r2.innerHTML += `<iframe scrolling="no" src="${url2}"></iframe>`;
       dwnbtn.style.backgroundColor = "#ff255c";
     } else if (link.value.indexOf("https://youtube.com/shorts/") != -1) {
@@ -42,7 +43,7 @@ function download() {
         "https://youtu.be/"
       );
       var u2 = u.replace("?feature=share", "");
-      var url2 = `https://loader.to/api/button/?url=${u2}&f=${format.value}`;
+      var url2 = `https://loader.to/api/button/?url=${u2}&f=${format.value}&color=${buttonColor}&css=${css}`;
       r2.innerHTML += `<iframe scrolling="no" src="${url2}"></iframe>`;
       dwnbtn.style.backgroundColor = "#ff255c";
     }
