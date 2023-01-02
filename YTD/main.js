6ecd1c81f09c197faa637f393475356ce7a4bdfa
Manoj Joshi <hello_manoj@outlook.com>
2023-01-02 11:18:39 +0545

    updated 2

--- YTD/main.js
+++ YTD/main.js
@@ -6,30 +6,41 @@ var r2 = document.querySelector(".result2");
 var rm = document.getElementById("rmsg");
 
 function download() {
-  if (link.value != "") {    
+  if (link.value != "") {
     rm.style.visibility = "visible";
     if (link.value.indexOf("https://youtu.be/") != -1) {
-      var url1 = link.value.replace("https://youtu.be/","https://www.youtube.com/embed/");
-      r1.innerHTML = `<iframe id="myframe" width="100%" height="100%" src="${url1}" encrypted-media; allowfullscreen></iframe>`;
+      var url1 = link.value.replace(
+        "https://youtu.be/",
+        "https://www.youtube.com/embed/"
+      );
+      r1.innerHTML = `<iframe id="myframe" width="105%" height="105%" src="${url1}" encrypted-media; allowfullscreen></iframe>`;
       var url2 = `https://loader.to/api/button/?url=${link.value}&f=${format.value}`;
       r2.innerHTML += `<iframe scrolling="no" src="${url2}"></iframe>`;
       dwnbtn.style.backgroundColor = "#ff255c";
-    } 
-
-    else if (link.value.indexOf("https://www.youtube.com/watch?v=") != -1) {
-      var url1 = link.value.replace("https://www.youtube.com/watch?v=","https://www.youtube.com/embed/");
-      r1.innerHTML = `<iframe id="myframe" width="100%" height="100%" src="${url1}" encrypted-media; allowfullscreen></iframe>`;
-      var u = link.value.replace("https://www.youtube.com/watch?v=","https://youtu.be/");
+    } else if (link.value.indexOf("https://www.youtube.com/watch?v=") != -1) {
+      var url1 = link.value.replace(
+        "https://www.youtube.com/watch?v=",
+        "https://www.youtube.com/embed/"
+      );
+      r1.innerHTML = `<iframe id="myframe" width="105%" height="105%" src="${url1}" encrypted-media; allowfullscreen></iframe>`;
+      var u = link.value.replace(
+        "https://www.youtube.com/watch?v=",
+        "https://youtu.be/"
+      );
       var url2 = `https://loader.to/api/button/?url=${u}&f=${format.value}`;
       r2.innerHTML += `<iframe scrolling="no" src="${url2}"></iframe>`;
       dwnbtn.style.backgroundColor = "#ff255c";
-    } 
-    
-    else if (link.value.indexOf("https://youtube.com/shorts/") != -1) {
-      var url1 = link.value.replace("https://youtube.com/shorts/","https://www.youtube.com/embed/");
+    } else if (link.value.indexOf("https://youtube.com/shorts/") != -1) {
+      var url1 = link.value.replace(
+        "https://youtube.com/shorts/",
+        "https://www.youtube.com/embed/"
+      );
       var u3 = url1.replace("?feature=share", "");
-      r1.innerHTML = `<iframe id="myframe" width="100%" height="100%" src="${u3}" encrypted-media; allowfullscreen></iframe>`;
-      var u = link.value.replace("https://youtube.com/shorts/","https://youtu.be/");
+      r1.innerHTML = `<iframe id="myframe" width="105%" height="105%" src="${u3}" encrypted-media; allowfullscreen></iframe>`;
+      var u = link.value.replace(
+        "https://youtube.com/shorts/",
+        "https://youtu.be/"
+      );
       var u2 = u.replace("?feature=share", "");
       var url2 = `https://loader.to/api/button/?url=${u2}&f=${format.value}`;
       r2.innerHTML += `<iframe scrolling="no" src="${url2}"></iframe>`;
@@ -37,5 +48,3 @@ function download() {
     }
   }
 }
-
-
