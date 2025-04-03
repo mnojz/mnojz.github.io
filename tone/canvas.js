119a19876a107621740f6f67a44b4592a8e2125b
Manoj Joshi <manoj.joshi.public@gmail.com>
2025-04-03 20:44:31 +0545

    big upgrade.

--- tone/canvas.js
+++ tone/canvas.js
@@ -1,6 +1,5 @@
-
 const canvas = document.getElementById("canvas");
-/** @type {CanvasRenderingContext2D} */
+// /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
 canvas.height = canvas.offsetHeight;
@@ -12,34 +11,30 @@ ctx.setTransform(1, 0, 0, -1, canvas.width / 2, canvas.height / 2);
 //=======================================================================
 //=======================================================================
 
-
 let λ = 0.05; //wavelength
 let f = 0.6; //frequency
 let Φ = 0; //phase initialize
 
-
-
 function animate() {
   requestAnimationFrame(animate);
-  ctx.clearRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
-  
-  let A = document.getElementById('gain').value; //intensity of wave
-  let f = document.getElementById('freq').value; //frequency of wave
-  
-  A = (f==0)?0:A;
+  ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
 
-  ctx.strokeStyle = "#00ff95";
+  let f = document.getElementById("freq-slider").value; //frequency of wave
+  let power = document.getElementById("pwr").checked;
+
+  console.log(power);
+
+  A = f == 0 || !power ? 0 : 80;
+
+  ctx.strokeStyle = "#00aeff";
   ctx.lineWidth = 2;
   ctx.beginPath();
-  ctx.moveTo(-canvas.width/2-1, 0);
-  for (let i = -canvas.width/2-1; i < canvas.width/2; i++) {
+  ctx.moveTo(-canvas.width / 2 - 1, 0);
+  for (let i = -canvas.width / 2 - 1; i < canvas.width / 2; i++) {
     ctx.lineTo(i, Math.sin(i * λ + Φ) * A);
   }
   ctx.stroke();
-  Φ+=(f/100);
-  λ=(f/10000);
-
+  Φ += f / 100;
+  λ = f / 10000;
 }
 animate();
-
-
--- tone/tone.js
+++ tone/tone.js
@@ -1,62 +1,64 @@
-const powerBtn = document.querySelector("#pwr");
-const ind = document.querySelector(".ind");
-const gainValue = document.getElementById("gain");
-const freqValue = document.getElementById("freq");
-const display = document.querySelector(".display");
+const powerBtn = document.getElementById("pwr");
+const freqValue = document.getElementById("freq-slider");
+const freqEntry = document.getElementById("freq-entry");
 
-let on = false;
+const freqIncrement = document.querySelector(".forward");
+const freqDecrement = document.querySelector(".backward");
+
 let audioCtx;
 let oscillator;
-let gainNode;
 
 powerBtn.onclick = () => {
-  if (!on) {
+  if (powerBtn.checked) {
     audioCtx = new AudioContext();
     oscillator = new OscillatorNode(audioCtx);
-    gainNode = new GainNode(audioCtx);
     oscillator.type = "sine";
     oscillator.frequency.value = parseInt(freqValue.value);
-    oscillator.connect(gainNode);
-    gainNode.gain.value = gainValue.value / 100;
-    gainNode.connect(audioCtx.destination);
+    oscillator.connect(audioCtx.destination);
     oscillator.start(0);
     console.log("engine starting");
-    on = !on;
-    powerBtn.style.color = "#00ff95";
-    powerBtn.style.borderColor = "#00ff95";
-    ind.style.backgroundColor = "#00ff95";
+
     freqValue.removeAttribute("disabled");
-    gainValue.removeAttribute("disabled");
+    freqEntry.removeAttribute("disabled");
+    freqIncrement.removeAttribute("disabled");
+    freqDecrement.removeAttribute("disabled");
   } else {
     audioCtx.close();
     console.log("engine shutting down");
-    on = !on;
-    powerBtn.style.color = "#d3d3d38f";
-    powerBtn.style.borderColor = "#d3d3d38f";
-    ind.style.backgroundColor = "#00ff9575";
-    freqValue.setAttribute("disabled","true");
-    gainValue.setAttribute("disabled","true");
+    freqValue.setAttribute("disabled", "true");
   }
 };
 
-gainValue.oninput = function () {
-  console.log(parseFloat(this.value / 100));
-  gainNode.gain.setValueAtTime(gainNode.gain.value, audioCtx.currentTime);
-  gainNode.gain.linearRampToValueAtTime(
-    parseFloat(this.value / 100),
-    audioCtx.currentTime + 0.1
-  );
+freqValue.oninput = function () {
+  oscillator.frequency.setValueAtTime(oscillator.frequency.value, audioCtx.currentTime);
+  oscillator.frequency.linearRampToValueAtTime(this.value, audioCtx.currentTime + 0.1);
+  freqEntry.value = parseInt(freqValue.value);
 };
 
-freqValue.oninput = function () {
-  console.log(parseInt(this.value));
-  oscillator.frequency.setValueAtTime(
-    oscillator.frequency.value,
-    audioCtx.currentTime
-  );
-  oscillator.frequency.linearRampToValueAtTime(
-    this.value,
-    audioCtx.currentTime + 0.1
-  );
-  display.innerHTML = parseInt(freqValue.value) + " Hz";
+freqEntry.oninput = function () {
+  oscillator.frequency.setValueAtTime(oscillator.frequency.value, audioCtx.currentTime);
+  oscillator.frequency.linearRampToValueAtTime(this.value, audioCtx.currentTime + 0.1);
+  if (freqEntry.value) {
+    freqValue.value = parseInt(freqEntry.value);
+  } else {
+    freqValue.value = 0;
+  }
 };
+
+function decreaseFrequency() {
+  if (freqValue.value > 0) {
+    freqEntry.value--;
+    freqValue.value--;
+    oscillator.frequency.setValueAtTime(oscillator.frequency.value, audioCtx.currentTime);
+    oscillator.frequency.linearRampToValueAtTime(freqValue.value, audioCtx.currentTime + 0.1);
+  }
+}
+
+function increaseFrequency() {
+  if (freqValue.value < 20000) {
+    freqEntry.value++;
+    freqValue.value++;
+    oscillator.frequency.setValueAtTime(oscillator.frequency.value, audioCtx.currentTime);
+    oscillator.frequency.linearRampToValueAtTime(freqValue.value, audioCtx.currentTime + 0.1);
+  }
+}
