8fdba0d0b5a86dbc011b54e71df8705e3e531c43
Manoj Joshi <manoj.joshi.public@gmail.com>
2025-09-30 09:11:45 +0545

    Delete signature directory

--- signature/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let path = document.querySelectorAll(".path");
-
-  path.forEach((item, index) => {
-    let length = item.getTotalLength();
-    item.style.strokeDasharray = length;
-    item.style.strokeDashoffset = length;
-  });
-});
